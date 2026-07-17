a2415616dbdbb5c3ebd6f5d207575cf0b86d442f
Đinh Công An <adche186895@fpt.edu.vn>
2026-07-18 04:24:55 +0700

    eureka-server

---
+++ fu_car_renting_system_andche186895/eureka-server/src/test/java/com/fptu/fucarrenting/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fptu.fucarrenting.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
